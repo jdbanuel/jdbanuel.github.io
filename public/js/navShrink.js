cc86bcb74f0386d32159b3a53f3725a1c55f9e2d
James Banuelos <jamesdb1995@gmail.com>
2019-05-29 22:06:01 -0700

    changing navbar text color and hover colors

--- public/js/navShrink.js
+++ public/js/navShrink.js
@@ -1,8 +1,10 @@
 $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
         $('nav').addClass('shrink');
+        $('.navInnerContent').addClass('list-shrink');
     }
     else {
         $('nav').removeClass('shrink');
+        $('.navInnerContent').removeClass('list-shrink');
     }
 });
